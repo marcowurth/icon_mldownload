045fde69e970811d6a18805ca0dd68d9a95809ff
Marco Wurth <marco.wurth@partner.kit.edu>
2023-03-10 03:35:59 +0100

    Add initial scripts

---
+++ scripts/download_forecast.py
@@ -0,0 +1,197 @@
+#########################################
+###  container for various functions  ###
+#########################################
+
+import os
+import fnmatch
+import datetime
+import bz2
+
+from requests import Session
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+from cdo import Cdo
+
+########################################################################
+########################################################################
+########################################################################
+
+def download(url, filename, path):
+    session = Session()
+    session.mount('https://', HTTPAdapter(max_retries=Retry(total=10, backoff_factor=0.5)))
+    r = session.get(url + filename, timeout=120)
+    with open(path['base'] + path['subdir'] + filename, 'wb') as file:
+        file.write(r.content)
+    r.close()
+    return 1
+
+########################################################################
+########################################################################
+########################################################################
+
+def unzip(path, filename):
+    newfilename = filename[:-4]     # cut file ending
+    with open(path['base'] + path['subdir'] + newfilename, 'wb') as unzippedfile:
+        with open(path['base'] + path['subdir'] + filename, 'rb') as zippedfile:
+            decompressor = bz2.BZ2Decompressor()
+            for datapart in iter(lambda : zippedfile.read(100 * 1024), b''):
+                try:
+                    unzippedfile.write(decompressor.decompress(datapart))
+                except OSError:
+                    try:
+                        unzippedfile.write(decompressor.decompress(datapart))
+                    except OSError:
+                        try:
+                            unzippedfile.write(decompressor.decompress(datapart))
+                        except OSError:
+                            try:
+                                unzippedfile.write(decompressor.decompress(datapart))
+                            except OSError:
+                                unzippedfile.write(decompressor.decompress(datapart))
+
+    os.remove(path['base'] + path['subdir'] + filename)
+    return newfilename
+
+########################################################################
+########################################################################
+########################################################################
+
+# this function merges all mlevel gribfiles to one gribfile with cdo #
+
+def merge_mlevel_files_to_one_grib(path, grib_matchname, merged_filename):
+
+    cdo_module = Cdo()
+    cdo_module.merge(input = path['base'] + path['subdir'] + grib_matchname,
+                     output = path['base'] + path['subdir'] + merged_filename)
+
+    for gribfile in fnmatch.filter(os.listdir(path['base'] + path['subdir']), grib_matchname):
+        os.remove(path['base'] + path['subdir'] + gribfile)
+
+    return
+
+########################################################################
+########################################################################
+########################################################################
+
+def interpolate_icosahedral_to_latlon(path, merged_filename, latlon_filename, latlon_resolution,
+                                      model, output_as_netcdf):
+
+    # interpolates icosahedral to regular latlon grid and save as netcdf4 file #
+
+    if model == 'icon-global-det':
+        targetgridfile = 'target_grid_global_latlon_{:.3f}.txt'.format(latlon_resolution)
+        weightsfile = 'weights_con1_{}_icosahedral_to_latlon_{:.3f}.nc'.format(model, latlon_resolution)
+    if output_as_netcdf:
+        options_out = '-f nc'
+    else:
+        options_out = ''
+
+    cdo_module = Cdo()
+    cdo_module.remap(path['base'] + path['grid'] + targetgridfile + ',' + path['base'] + path['grid'] + weightsfile,
+                     input = path['base'] + path['subdir'] + merged_filename,
+                     output = path['base'] + path['subdir'] + latlon_filename,
+                     options=options_out)
+
+    return
+
+########################################################################
+########################################################################
+########################################################################
+
+def calc_latest_run_time(model):
+
+    # keep always on track with dwd / ecmwf-open-data update times #
+
+    if model == 'icon-eu-eps':
+        update_times_utc = [4+30/60, 9+49/60, 16+30/60, 21+49/60]
+    elif model == 'icon-global-eps':
+        update_times_utc = [4+24/60, 16+24/60]
+    elif model == 'icon-eu-det':
+        update_times_utc = [3+47/60, 9+41/60, 15+47/60, 21+41/60]
+    elif model == 'icon-global-det':
+        update_times_utc = [4+20/60, 15+29/60]
+    elif model == 'ecmwf-hres':
+        update_times_utc = [7+55/60, 12+45/60, 19+55/60, 24+45/60]
+
+
+    datenow  = datetime.datetime.now().date()
+    timenow  = datetime.datetime.now(datetime.timezone.utc).time()
+    run_year  = datenow.year
+    run_month = datenow.month
+    run_day   = datenow.day
+    run_time_float = timenow.hour + timenow.minute / 60
+
+    if run_time_float < update_times_utc[0]:
+        run_year, run_month, run_day = go_back_one_day(run_year, run_month, run_day)
+
+    if len(update_times_utc) == 2:
+        if   run_time_float >= update_times_utc[0] and run_time_float < update_times_utc[1]: run_hour = 0
+        elif run_time_float >= update_times_utc[1]  or run_time_float < update_times_utc[0]: run_hour = 12
+        else: exit()
+    elif len(update_times_utc) == 4:
+        if update_times_utc[3] > 24 and run_time_float < update_times_utc[3] - 24:
+            run_time_float += 24
+        if   run_time_float >= update_times_utc[0] and run_time_float < update_times_utc[1]: run_hour = 0
+        elif run_time_float >= update_times_utc[1] and run_time_float < update_times_utc[2]: run_hour = 6
+        elif run_time_float >= update_times_utc[2] and run_time_float < update_times_utc[3]: run_hour = 12
+        elif run_time_float >= update_times_utc[3]: run_hour = 18
+        elif run_time_float < update_times_utc[0]: run_hour = 18
+        else: exit()
+
+    date = dict(year = run_year, month = run_month, day = run_day, hour = run_hour)
+    return date
+
+########################################################################
+########################################################################
+########################################################################
+
+def go_back_one_day(run_year, run_month, run_day):
+    if run_day >= 2: run_day -= 1
+    else:
+        # run_day is 1
+        if run_year % 4 == 0:
+            # schaltjahre/leap years being considered
+            days_of_month = [31,29,31,30,31,30,31,31,30,31,30,31]
+        else:
+            days_of_month = [31,28,31,30,31,30,31,31,30,31,30,31]
+        if run_month >= 2:
+            run_month -= 1
+            run_day = days_of_month[run_month-1]
+        else:
+            # run_month is 1
+            run_year -= 1
+            run_month = 12
+            run_day = days_of_month[run_month-1]
+
+    return run_year, run_month, run_day
+    
+########################################################################
+########################################################################
+########################################################################
+
+def get_timeshift():
+    datenow  = datetime.datetime.now()
+    dayofyear_now = (datenow-datetime.datetime(datenow.year,1,1)).days + 1
+
+    #                     2019    2020    2021    2022    2023
+    changedate_spring = [[3, 31],[3, 29],[3, 28],[3, 27],[3, 26]]
+    changedate_autumn = [[10,27],[10,25],[10,31],[10,30],[10,29]]
+    year_index = datenow.year - 2019
+    date_spring = datetime.datetime(datenow.year, changedate_spring[year_index][0],
+                                    changedate_spring[year_index][1], 0, 0)
+    dayofyear_spring = (date_spring - datetime.datetime(datenow.year,1,1)).days + 1
+    date_autumn = datetime.datetime(datenow.year, changedate_autumn[year_index][0],
+                                    changedate_autumn[year_index][1], 0, 0)
+    dayofyear_autumn = (date_autumn - datetime.datetime(datenow.year,1,1)).days + 1
+
+    if dayofyear_now >= dayofyear_spring and dayofyear_now < dayofyear_autumn:
+        timeshift = 2
+    else:
+        timeshift = 1
+
+    return timeshift
+
+########################################################################
+########################################################################
+########################################################################
+
---
+++ scripts/download_ml_icon.py
@@ -0,0 +1,117 @@
+
+import time
+import os
+import sys
+import datetime
+
+import distributed
+
+from download_forecast import calc_latest_run_time
+from download_global_ml_vars import download_global_ml_vars
+
+
+def main():
+
+    # settings #
+
+    fcst_hours_list = list(range(0, 72+1, 3))
+    fcst_hours_list = [12]
+
+    mlevels = list(range(58, 120+1, 1))
+
+    latlon_resolution = 0.25    # only relevant when interpolation weights available
+
+    #parallel = True     # parallisation works over fcst_hours, not vars
+    parallel = False
+
+
+    # get latest run time #
+
+    run = calc_latest_run_time('icon-global-det')
+    #run = dict(year = 2023, month = 2, day = 16, hour = 0)
+    #run['hour'] = 0
+
+    print('download and calculate from run_{}{:02}{:02}{:02}'.format(
+           run['year'], run['month'], run['day'], run['hour']))
+
+
+    # create paths #
+
+    path = dict(base = 'here/goes/the/absolute/path/to/folder/containing/scripts/',
+                grid = 'grid/',
+                data = 'data/forecasts/run_{}{:02}{:02}{:02}'.format(
+                        run['year'], run['month'], run['day'], run['hour']))
+    if not os.path.isdir(path['base'] + path['data']):
+        os.mkdir(path['base'] + path['data'])
+    path['data'] = path['data'] + '/'
+
+    for varfolder in ['p','t','qv','u','v','w']:
+        if not os.path.isdir(path['base'] + path['data'] + varfolder):
+            os.mkdir(path['base'] + path['data'] + varfolder)
+ 
+
+
+    # create tasks #
+
+    tasks = []
+    task_idx = 0
+    for fcst_hour in fcst_hours_list:
+        tasks.append([parallel, task_idx, path, run, fcst_hour, mlevels, latlon_resolution])
+        task_idx += 1
+
+
+    if parallel:
+        # option: parallelized #
+
+        num_workers = 1
+        num_threads = 4
+        client = distributed.Client(name='icon_mlevel',
+                                    n_workers=num_workers, threads_per_worker=num_threads,
+                                    dashboard_address='127.0.0.1:5000')
+
+        futures = client.map(download_global_ml_vars, tasks, pure=True)
+        futures = client.gather(futures)
+        client.close()
+
+        print('finished')
+        if all(futures):
+            print('all tasks successful')
+        else:
+            print('the following tasks were not successful:')
+            for i, future in enumerate(futures):
+                if not future:
+                    print(tasks[i][:2])
+
+    else:
+        # option: unparallelized/serial #
+
+        for task in tasks:
+            future = download_global_ml_vars(task)
+
+
+    # remove last run folder
+
+    #run_datetime = datetime.datetime(run['year'], run['month'], run['day'], run['hour'])
+    #last_run_datetime = run_datetime - datetime.timedelta(seconds=60*60*12)
+    #pathtoremove = '{}{}run_{}{:02}{:02}{:02}'.format(
+    #                path['base'], path['data'][:-14],
+    #                last_run_datetime.year, last_run_datetime.month, last_run_datetime.day, last_run_datetime.hour)
+    #os.system('rm -rf ' + pathtoremove)
+
+    return
+
+########################################################################
+########################################################################
+########################################################################
+
+if __name__ == '__main__':
+    t1 = time.time()
+    main()
+    t2 = time.time()
+    delta_t = t2-t1
+    if delta_t < 60:
+        print('total script time:  {:.1f}s'.format(delta_t))
+    elif 60 <= delta_t <= 3600:
+        print('total script time:  {:.0f}min{:.0f}s'.format(delta_t//60, delta_t-delta_t//60*60))
+    else:
+        print('total script time:  {:.0f}h{:.1f}min'.format(delta_t//3600, (delta_t-delta_t//3600*3600)/60))
